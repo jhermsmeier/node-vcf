ba1c1995339de48256181bf07342970129d0975a
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-06-18 20:05:29 +0200

    Rename test/data/version-*.vcf => vcard-*.vcf

--- benchmark/index.js
+++ benchmark/index.js
@@ -3,7 +3,7 @@ var vCard = require( '..' )
 
 ;[ '2.1', '3.0', '4.0' ].forEach( function( version ) {
 
-  var filename = __dirname + '/../test/data/version-' + version + '.vcf'
+  var filename = __dirname + '/../test/data/vcard-' + version + '.vcf'
   var input = fs.readFileSync( filename, 'utf8' )
 
   suite( 'vCard ' + version, function() {
