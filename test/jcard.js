771c1650f7c08268a8c0f1ed97046dad1ea4864d
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-08-06 14:04:16 +0200

    Update lib/vcard: Add `.toJCard([version='4.0'])`

--- test/jcard.js
+++ test/jcard.js
@@ -17,6 +17,12 @@ suite( 'vCard', function() {
       assert.deepEqual( card.toJSON(), data )
     })
 
+    test( 'toJCard', function() {
+      var data = require( './data/jcard' )
+      var card = vCard.fromJSON( data )
+      assert.deepEqual( card.toJCard(), data )
+    })
+
   })
 
 })
