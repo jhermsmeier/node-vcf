11dd4ff487a704a85b2c41f17a6aa7fc4559bcf9
Martin Landes <martin.landes@smartcrm.de>
2019-08-23 16:17:17 +0200

    added test for parsing of quoted-printable values

---
+++ test/data/vcard-withQuotedPrintableEncoding.js
@@ -0,0 +1,22 @@
+module.exports =
+  'BEGIN:VCARD\r\n' +
+  'VERSION:4.0\r\n' +
+  'N:Gump;Forrest;;;\r\n' +
+  'FN:Forrest Gump\r\n' +
+  'ORG:Bubba Gump Shrimp Co.\r\n' +
+  'TITLE:Shrimp Man\r\n' +
+  'PHOTO;MEDIATYPE=image/gif:http://www.example.com/dir_photos/my_photo.gif\r\n' +
+  'TEL;TYPE=work,voice;VALUE=uri:tel:+11115551212\r\n' +
+  'TEL;TYPE=home,voice;VALUE=uri:tel:+14045551212\r\n' +
+  'item1.TEL;TYPE=home,voice;VALUE=uri:tel:+14045551213\r\n' +
+  'ADR;TYPE=work;LABEL="100 Waters Edge\\nBaytown, LA 30314\\nUnited States of A\r\n' +
+  ' merica":;;100 Waters Edge;Baytown;LA;30314;United States of America\r\n' +
+  'ADR;TYPE=home;LABEL="42 Plantation St.\\nBaytown, LA 30314\\nUnited States of\r\n' +
+  '  America":;;42 Plantation St.;Baytown;LA;30314;United States of America\r\n' +
+  'EMAIL:forrestgump@example.com\r\n' +
+  'NOTE;ENCODING=QUOTED-PRINTABLE;CHARSET=utf-8:foobar foobar foobar foobar fo=\n' +
+  'obar foobar foobar foobar foobar=0Afoobar foobar foobar foobar foobar fooba=\n' +
+  'r=0Afoobar foobar foobar foobar foobar foobar=0Afoobar foobar foobar foobar=\n' +
+  ' foobar foobar foobar foobar foobar\r\n' +
+  'REV:20080424T195243Z\r\n' +
+  'END:VCARD\r\n';
