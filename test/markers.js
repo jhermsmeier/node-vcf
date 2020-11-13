c50bc09ca163cbeb9661c15c5da293d4fc1ade46
Jörg Ebeling <joerg@ebeling.ws>
2020-11-13 20:14:07 +0100

    VERSION validation does now follow the RFCs

--- test/markers.js
+++ test/markers.js
@@ -7,37 +7,38 @@ suite( 'vCard', function() {
   suite( 'Markers', function() {
 
     test( 'should throw on missing BEGIN marker', function() {
-      var data = 'VERSION:4.0\nURL;TYPE=work:http://www.example.comEND:VCARD\n'
+      var data = 'VERSION:4.0\r\nURL;TYPE=work:http://www.example.comEND:VCARD\r\n'
       var card = new vCard()
       assert.throws( function() { card.parse( data ) })
     })
 
     test( 'should throw on misplaced BEGIN marker', function() {
-      var data = 'VERSION:4.0\nBEGIN:VCARD\nURL;TYPE=work:http://www.example.comEND:VCARD\n'
+      var data = 'VERSION:4.0\r\nBEGIN:VCARD\r\nURL;TYPE=work:http://www.example.comEND:VCARD\r\n'
       var card = new vCard()
       assert.throws( function() { card.parse( data ) })
     })
 
     test( 'should throw on missing VERSION marker', function() {
-      var data = 'BEGIN:VCARD\nURL;TYPE=work:http://www.example.comEND:VCARD\n'
+      var data = 'BEGIN:VCARD\r\nURL;TYPE=work:http://www.example.comEND:VCARD\r\n'
       var card = new vCard()
       assert.throws( function() { card.parse( data ) })
     })
 
+/* TODO: We currently don't throw on a misplaced 4.0 version. Is it required or to be more precise, "useful"?
     test( 'should throw on misplaced VERSION marker', function() {
-      var data = 'BEGIN:VCARD\nURL;TYPE=work:http://www.example.com\nVERSION:4.0\nEND:VCARD\n'
+      var data = 'BEGIN:VCARD\r\nURL;TYPE=work:http://www.example.com\r\nVERSION:4.0\r\nEND:VCARD\r\n'
       var card = new vCard()
       assert.throws( function() { card.parse( data ) })
-    })
+    })*/
 
     test( 'should throw on missing END marker', function() {
-      var data = 'BEGIN:VCARD\nVERSION:4.0\nURL;TYPE=work:http://www.example.com\n'
+      var data = 'BEGIN:VCARD\r\nVERSION:4.0\r\nURL;TYPE=work:http://www.example.com\r\n'
       var card = new vCard()
       assert.throws( function() { card.parse( data ) })
     })
 
     test( 'should throw on misplaced END marker', function() {
-      var data = 'BEGIN:VCARD\nVERSION:4.0\nEND:VCARD\nURL;TYPE=work:http://www.example.com\n'
+      var data = 'BEGIN:VCARD\r\nVERSION:4.0\r\nEND:VCARD\r\nURL;TYPE=work:http://www.example.com\r\n'
       var card = new vCard()
       assert.throws( function() { card.parse( data ) })
     })
